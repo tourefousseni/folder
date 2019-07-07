1fab869747f67f50d45b1105d12966bb93e51257
fulani <tourefousseni1@gmail.com>
2019-07-07 21:16:35 +0000

    add library psycopg2 name db.py

---
+++ k/db.py
@@ -0,0 +1,23 @@
+import psycopg2
+# connect to the database
+
+con = psycopg2.connect(
+    host="localhost",
+    database="kala1",
+    user="fulani",
+    password="",)
+
+# the cursor
+cur = con.cursor()
+
+#cur.execute("insert into k_person(id, prenom) values (%s, %s)", (4, "fili"))
+# execute query
+cur.execute("select * from k_person")
+
+rows = cur.fetchall()
+for r in rows:
+    print(f" {r[0]} {r[1]} {r[2]} ")
+
+
+# close the connection
+con.close()
