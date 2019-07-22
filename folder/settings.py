1770891138a5f02dad74c4d4b2202f4bc36f80c3
fulani <tourefousseni1@gmail.com>
2019-07-22 23:57:49 +0000

    add staticfiles

--- folder/settings.py
+++ folder/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'k.apps.KConfig',
 
 ]
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/Home/PycharmProjects/folder2/folder/'
