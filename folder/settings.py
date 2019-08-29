d05f6fd724b558a8295d8e031e8dbe7ab7f6f03e
fulani <tourefousseni1@gmail.com>
2019-08-29 13:24:14 +0000

    refaire

--- folder/settings.py
+++ folder/settings.py
@@ -126,5 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = '/Users/Home/PycharmProjects/folder2/folder/'
 STATIC_URL = '/static/'
-STATIC_ROOT = '/Users/Home/PycharmProjects/folder2/folder/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "media")
