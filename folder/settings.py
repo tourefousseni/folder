6c51ba9d6b76d616d88a0392544bc65bccffb4ab
fulani <tourefousseni1@gmail.com>
2019-11-26 22:21:41 +0000

    ADD ADDRESS IP ON ALLOWS HOST

--- folder/settings.py
+++ folder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'obeehkfa%tp_(@h*9vpd%ar-exo+vqz^kollj*2_xuasm&(*z5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['167.99.170.85']
 
 
 # Application definition
